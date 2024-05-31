b076fec7775f86c671397159cf79648c8837e092
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-31 14:08:31 +0530

    todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
+  const [todo, seTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = () => {
+    setTodos([...todos, { todo, isCompleted: false }])
+    seTodo("");
+  }
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = () => {
+
+  }
 
   return (
     <>
       <Navbar />
       <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
-        <div className="addTodo">
+        <div className="addTodo my-5">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" />
-          <button className='bg-violet-800 hover:bg-violet-950 p-3 py-1 text-white'>Add</button>
+          <input type="text" className='w-80' />
+          <button onClick={handleAdd} className='bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6'>Add</button>
         </div>
         <h2 className='text-lg font-bold'>Your Todos</h2>
         <div className="todos">
-          <div className="todo flex justify-between">
+          <div className="todo flex">
             <div className="text">
-              Lorem ipsum dolor sit, amet consectetur adipisicing.
+              {todo}
             </div>
             <div className="buttons">
-              <button>Edit</button>
-              <button>Delete</button>
+              <button onClick={handleEdit} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-2">Delete</button>
+              <button onClick={handleDelete} className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-2">Edit</button>
             </div>
           </div>
         </div>
